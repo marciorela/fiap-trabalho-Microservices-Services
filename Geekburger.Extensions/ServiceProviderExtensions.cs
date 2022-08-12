b16d924a8ae40c4d8a48aeb2e8275f06dd9132ba
Marcio Rela <marciorela75@gmail.com>
2022-08-11 21:17:16 -0300

    Projeto de Extensions

---
+++ Geekburger.Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Geekburger.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static async Task ExecuteAsync<T>(this IServiceProvider svc, Func<T, Task> func)
+        {
+            using var scope = svc.CreateScope();
+
+            var service = scope.ServiceProvider.GetService<T>();
+            if (service is not null)
+            {
+                await func(service);
+            }
+        }
+
+        public static void Execute<T>(this IServiceProvider svc, Action<T> func)
+        {
+            using var scope = svc.CreateScope();
+
+            var service = scope.ServiceProvider.GetService<T>();
+            if (service is not null)
+            {
+                func(service);
+            }
+        }
+    }
+}
